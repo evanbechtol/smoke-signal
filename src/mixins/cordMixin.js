b9d83436cf82541756cc1321e1e28036a672084c
eevabec <evan.bechtol@ericsson.com>
2019-02-21 11:20:13 -0600

    Cord Mixin Update
    - Add API endpoints for cord routes

--- src/mixins/cordMixin.js
+++ src/mixins/cordMixin.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3000";
+
 export const cordMixin = {
   computed: {
     selectedCord: {
@@ -8,5 +12,131 @@ export const cordMixin = {
         this.$store.commit("selectedCord", payload);
       }
     }
+  },
+  methods: {
+    /**
+     * @description Attempts to create a new cord using the body provided
+     * @param data {object} Body to be used to crete a new cord
+     * @returns {Promise} Returns promise for request being generated
+     */
+    createCord(data) {
+      const route = `cords`;
+      const options = {
+        method: "POST",
+        url: `${baseUrl}/${route}`,
+        data
+      };
+      return makeRequest(options);
+    },
+    /**
+     * @description Attempts to delete a cord matching the provided Object ID
+     * @param id {string} Object ID for the cord to delete
+     * @returns {Promise} Returns promise for request being generated
+     */
+    deleteCord(id) {
+      const route = `cords/${id}`;
+      const options = {
+        method: "DELETE",
+        url: `${baseUrl}/${route}`
+      };
+      return makeRequest(options);
+    },
+    /**
+     * @description
+     * @param limit {number} Optional - Controls page size (number of objects returned)
+     * @param skip {number} Optional - Offset for pagination
+     * @param query [string | object] Optional - Query to be used to retrieve data
+     * @returns {Promise} Returns promise for request being generated
+     */
+    getCords(limit = 20, skip = 0, query = null) {
+      const route = `cords?limit=${limit}&skip=${skip}&query=${
+        query === null ? "" : query
+      }`;
+      const options = {
+        method: "GET",
+        url: `${baseUrl}/${route}`
+      };
+      return makeRequest(options);
+    },
+    /**
+     * @description Attempts to retrieve a cord by the Object ID provided
+     * @param id {string} Object ID for the cord to retrieve
+     * @returns {Promise} Returns promise for request being generated
+     */
+    getCordById(id) {
+      const route = `cords/${id}`;
+      const options = {
+        method: "GET",
+        url: `${baseUrl}/${route}`
+      };
+      return makeRequest(options);
+    },
+    /**
+     * @description Attempts to retrieve all cords with the status provided
+     * @param status {string} Case-sensitive Status to search for
+     * @returns {Promise} Returns promise for request being generated
+     */
+    getCordsByStatus(status) {
+      const route = `cords/status/${status}`;
+      const options = {
+        method: "GET",
+        url: `${baseUrl}/${route}`
+      };
+      return makeRequest(options);
+    },
+    /**
+     * @description Attempts to update the cord matching the provided Object ID
+     * @param id {string} Object ID for the cord to update
+     * @param data {object} Body to use when updating the cord
+     * @returns {Promise} Returns promise for request being generated
+     */
+    updateCord(id, data) {
+      const route = `cords/${id}`;
+      const options = {
+        method: "PUT",
+        url: `${baseUrl}/${route}`,
+        data
+      };
+      return makeRequest(options);
+    }
   }
 };
+
+/**
+ * @description
+ * @param options {object}
+ * @returns {Promise} Returns a promise for the axios request being made.
+ *   Will resolve if request was made successfully, will reject otherwise
+ */
+function makeRequest(options = null) {
+  const bodyMethods = ["POST", "PUT"];
+  const isBodyRequired = bodyMethods.includes(options.method);
+  const isValid = options && options.method && options.url;
+
+  return new Promise((resolve, reject) => {
+    if (isValid) {
+      if (isBodyRequired && !options.data) {
+        return reject(
+          // Request invalid; options object not valid to perform request
+          respondError("Request not made: required body property not provided")
+        );
+      } else {
+        axios(options)
+          .then(response => {
+            return resolve(response);
+          })
+          .catch(err => {
+            debugger;
+            return reject(err);
+          });
+      }
+    } else {
+      // Request invalid; options object not valid to perform request
+      return reject(respondError("Request not made: options object invalid"));
+    }
+  });
+}
+
+function respondError(message) {
+  return { success: false, error: message };
+}
