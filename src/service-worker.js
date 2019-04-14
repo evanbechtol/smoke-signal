e82574a1ea0f8ff40048290ff8c4d1b8d3fb5a3e
eevabec <evan.bechtol@ericsson.com>
2019-04-14 18:35:22 -0500

    Update service-worker fetch

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,19 +9,27 @@ self.addEventListener("message", msg => {
   }
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", event => {
+  // Let the browser do its default thing
+  // for non-GET requests.
+  if (event.request.method !== "GET") return;
+
+  // Prevent the default, and handle the request ourselves.
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request).then(function(res) {
-          return caches.open("dynamic").then(function(cache) {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        });
+    (async function() {
+      // Try to get the response from a cache.
+      const cache = await caches.open("dynamic-v1");
+      const cachedResponse = await cache.match(event.request);
+
+      if (cachedResponse) {
+        // If we found a match in the cache, return it, but also
+        // update the entry in the cache in the background.
+        event.waitUntil(cache.add(event.request));
+        return cachedResponse;
       }
-    })
+
+      // If we didn't find a match in the cache, use the network.
+      return fetch(event.request);
+    })()
   );
 });
